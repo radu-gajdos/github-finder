e7c0b67ae3d3f89a1554de18d85a5fae9049fd4d
Radu <radugajdos123@gmail.com>
2024-02-07 14:51:55 +0200

    creating-specific UserItem

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import { render } from "@testing-library/react";
 import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem"
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      id: 'id',
+      login: 'mojombo',
+      avatar_url: 'step="0.01"',
+      html_url: 'https://github.com/mojombo',
+    }
+  }
+
   render() {
     return (
       <div className="className">
         <Navbar icon="fa-brands fa-github"/>
+        <UserItem></UserItem>
       </div>
     );
   }
