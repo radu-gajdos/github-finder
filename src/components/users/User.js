e6470c0dced3b0ab9cce76e91f41463860a33fd6
Radu <radugajdos123@gmail.com>
2024-03-05 17:01:27 +0200

    completed the User component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, Component, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const User = ({ getUser, user, loading }) => {
   const { login } = useParams();
@@ -25,9 +26,71 @@ const User = ({ getUser, user, loading }) => {
   } = user;
 
   return (
-    <div>
-      <h1>{name}</h1>
-    </div>
+    <Fragment>
+      <Link to='/' className='btn btn-light'>
+        Back To Search
+      </Link>
+      Hireable:{' '}
+      {hireable ? (
+        <i className='fas fa-check text-success' />
+      ) : (
+        <i className='fas fa-times-circle text-danger' />
+      )}
+      <div className='card grid-2'>
+        <div className='all-center'>
+          <img
+            src={avatar_url}
+            className='round-img'
+            alt=''
+            style={{ width: '150px' }}
+          />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
+        </div>
+        <div>
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a href={html_url} className='btn btn-dark my-1'>
+            Visit Github Profile
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <Fragment>
+                  <strong>Username: </strong> {login}
+                </Fragment>
+              )}
+            </li>
+
+            <li>
+              {company && (
+                <Fragment>
+                  <strong>Company: </strong> {company}
+                </Fragment>
+              )}
+            </li>
+
+            <li>
+              {blog && (
+                <Fragment>
+                  <strong>Website: </strong> {blog}
+                </Fragment>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className='card text-center'>
+        <div className='badge badge-primary'>Followers: {followers}</div>
+        <div className='badge badge-success'>Following: {following}</div>
+        <div className='badge badge-light'>Public Repos: {public_repos}</div>
+        <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+      </div>
+    </Fragment>
   );
 };
 
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,27 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const UserItem = ({user: { login, html_url, avatar_url }}) => {
-    return (
-        <div className="card text-centre">
-            <img
-                src={avatar_url}
-                alt=""
-                className="round-img"
-                style={{ width: "60px" }}
-            />
-            <h3>{login}</h3>
-            <div>
-                <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
-                    More
-                </Link>
-            </div>
-        </div>
-    );
+const UserItem = ({ user: { login, html_url, avatar_url } }) => {
+  return (
+    <div className="card text-centre">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
+          More
+        </Link>
+      </div>
+    </div>
+  );
 };
 
-UserItem.defaultProps={
-    user: PropTypes.object.isRequired,
-}
+UserItem.propTypes = {
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    html_url: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default UserItem;
